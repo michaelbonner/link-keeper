95f343ab7792390410a092821fdc9de35d9a6950
Michael Bonner <michaelwbonner@gmail.com>
2017-07-02 15:20:03 -0600

    tag single remove unused things

--- resources/views/tag/show.blade.php
+++ resources/views/tag/show.blade.php
@@ -5,16 +5,7 @@
 @section('content')
 	<h2 class="title">{{ $tag->name }} Links:</h2>
 
-	<h3 class="subtitle">Indexes:</h3>
-	<div class="columns is-multiline">
-		@foreach( $links as $key => $link )
-			@if( $link->type == 'index' )
-				@component('components.simple-image-link', compact( 'link', 'is_tag' )) @endcomponent
-			@endif
-		@endforeach
-	</div>
-
-	<h3 class="subtitle">Singles:</h3>
+	<h3 class="subtitle">Links:</h3>
 	<div class="columns is-multiline">
 		@foreach( $links as $key => $link )
 			@if( $link->type != 'index' )
